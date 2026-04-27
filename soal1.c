e15681cbc0a098c125900de629fffa9a6e548514
JevanAbiellie <jevanabiellie@gmail.com>
2026-04-27 09:16:52 +0700

    soal1.c

--- soal1.c
+++ soal1.c
@@ -1,5 +1,74 @@
-#include <stdio.h>
+/** EL2008 Praktikum Pemecahan Masalah dengan Pemrograman 2025/2026
+ *   Modul               : 3 - Structures and Dynamic Arrays
+ *   Hari dan Tanggal    : Senin, 27 April 2026
+ *   Nama (NIM)          : Jevan Abiellie (13224056)
+ *   Nama File           : Soal1.c
+ *   Deskripsi           : mencari count, sort, dan median dari suatu list
+ * 
+ */
 
-int main(void) {
-    return 0;
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int *daftar = NULL;
+int jumlah = 0;
+int median;
+float mediun; 
+void tambah(){
+    int bilangan;
+    scanf("%d", &bilangan);
+    while (bilangan != -1){
+        jumlah++;
+        daftar = realloc(daftar, jumlah * sizeof(int));
+        daftar[jumlah-1] = bilangan;
+        scanf("%d", &bilangan);
+    }
+}
+
+void hitung_median(){
+    if (jumlah % 2 == 0){
+        int kiri = (jumlah-1)/2;
+        int kanan = (jumlah-1)/2 + 1;
+        median = (daftar[kiri] + daftar[kanan])/2;
+        mediun = median + 0.5;
+    }
+    else if (jumlah % 2 == 1){
+        int tengah = (jumlah-1)/2;
+        median = daftar[tengah];
+    }
+}
+
+void sort(){
+    for (int i = 0; i < jumlah-1; i++){
+        for (int j = 0; j < jumlah-1; j++){
+            if (daftar[j] > daftar[j+1]){
+                int temp = daftar[j+1];
+                daftar[j+1] = daftar[j];
+                daftar[j] = temp;
+            }
+        }
+    }
+}
+
+void output(){
+    printf("COUNT %d ", jumlah);
+    printf("SORTED ");
+    for (int i = 0; i < jumlah-1; i++){
+        printf("%d ", daftar[i]);
+    }
+    printf("%d ", daftar[jumlah-1]);
+    if(jumlah % 2 == 1){
+        printf("MEDIAN %g", median);
+    }
+    else if (jumlah %2 == 0){
+        printf("MEDIAN %0.1f", mediun);
+    }
+}
+
+int main(){
+    tambah();
+    sort();
+    hitung_median();
+    output();
 }
